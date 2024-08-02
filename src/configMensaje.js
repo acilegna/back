e2abc7e9bcc292f45662287656b762fb9d4c4ca5
acilegna <acilegna.airam88@gmail.com>
2024-08-01 23:00:03 -0600

    rese

--- src/configMensaje.js
+++ src/configMensaje.js
@@ -3,8 +3,9 @@ const nodemailer = require("nodemailer");
 module.exports = (formulario) => {
   var transporter = nodemailer.createTransport({
     host: "smtp.resend.com",  
-    port: 465,
     secure: true,
+    port: 465,
+    
     auth: {
       user: "resend", // Cambialo por tu email
       pass: "re_N3XRTEJu_49mNXGANgbmS91TgPsEGQcE5", // Cambialo por tu password re_N3XRTEJu_49mNXGANgbmS91TgPsEGQcE5
@@ -13,8 +14,8 @@ module.exports = (formulario) => {
 
   
   const mailOptions = {
-    from: ` <strong>${formulario.nombre}:</strong>`,
-    //from: `<onboarding@resend.dev>`,
+   // from: ` <strong>${formulario.nombre}:</strong>`,
+    from: `<onboarding@resend.dev>`,
     to: "m4x1miliano.2022@gmail.com ", // Cambia esta parte por el destinatario
     subject: "Email Portafolio",
     html: `
