50a82b7537eeb025ad7d095d74459e0070a4a7a4
acilegna <acilegna.airam88@gmail.com>
2024-08-03 20:51:08 -0600

    libreria resend

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
- 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const configMensaje = require('./src/configMensaje');
- 
+
 const app = express();
+
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-app.post('/formulario', (req, res) => {
- configMensaje(req.body);
- res.status(200).send();
-})
+app.post("/formulario", (req, res) => {
+  configMensaje(req.body);
+  res.status(200).send();
+});
 
-
-app.get("/", (req, res)=>{
-    res.send("pagina inicio desde indexxx")
+app.get("/", (req, res) => {
+  res.send("pagina inicio desde indexxx");
 });
 
 app.listen(3000, () => {
- console.log('Servidor corriendos ')
+  console.log("Servidor corriendos ");
 });
--- src/configMensaje.js
+++ src/configMensaje.js
@@ -1,3 +1,47 @@
+ 
+const {Resend} = require("resend");
+const instanceResend = new Resend('re_N3XRTEJu_49mNXGANgbmS91TgPsEGQcE5');
+ 
+ 
+
+module.exports  = (formulario) => {
+
+
+  const { data, error } =   instanceResend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['m4x1miliano.2022@gmail.com'],
+    subject: 'Hello World',
+    html: `
+  <strong>Nombre:</strong> ${formulario.nombre} <br/>
+  <strong>E-mail:</strong> ${formulario.email} <br/>
+  <strong>Mensaje:</strong> ${formulario.mensaje}
+  `,
+  });
+
+  if (error) {
+    return console.error({ error });
+  }
+
+  console.log({ data });
+ 
+ 
+
+ 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 const nodemailer = require("nodemailer");
 
 module.exports = (formulario) => {
@@ -33,4 +77,4 @@ module.exports = (formulario) => {
       console.log("Enviado! " + info.response);
     }
   });
-};
+}; */
