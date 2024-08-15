c3e3b294e73f1d28fec53b2c92ddbec0f7ea59a9
acilegna <acilegna.airam88@gmail.com>
2024-08-14 23:58:19 -0600

    changenodemail3

--- src/configMensaje.js
+++ src/configMensaje.js
@@ -25,39 +25,42 @@ module.exports = (formulario) => {
   
   
 
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-module.exports = (formulario) => {
-  var transporter = nodemailer.createTransport({
-    host: "smtp.resend.com",  
-    secure: true,
-    port: 465,
-    
+
+
+  module.exports = (formulario) => {
+    var transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-      user: "resend", // Cambialo por tu email
-      pass: "re_N3XRTEJu_49mNXGANgbmS91TgPsEGQcE5", // Cambialo por tu password re_N3XRTEJu_49mNXGANgbmS91TgPsEGQcE5
-    },
-  });
+    user: 'acilegna.airam88@gmail.com', // Cambialo por tu email
+    pass: 'nzql iljq cpbu ocle' // Cambialo por tu password
+    }
+    });
+
+
+  const mailOptions = {
+   
+   
+    from: ` <strong>${formulario.nombre}:</strong>`,
+ 
+    to: 'amgye2188@gmail.com', // Cambia esta parte por el destinatario
+    subject: 'Email Portafolio',
+    html: `
+    <strong>Nombre:</strong> ${formulario.nombre} <br/>
+    <strong>E-mail:</strong> ${formulario.email} <br/>
+    <strong>Mensaje:</strong> ${formulario.mensaje}
+    `
+    };
 
   
-  const mailOptions = {
-   // from: ` <strong>${formulario.nombre}:</strong>`,
-    from: `<onboarding@resend.dev>`,
-    to: "m4x1miliano.2022@gmail.com ", // Cambia esta parte por el destinatario
-    subject: "Email Portafolio",
-    html: `
-  <strong>Nombre:</strong> ${formulario.nombre} <br/>
-  <strong>E-mail:</strong> ${formulario.email} <br/>
-  <strong>Mensaje:</strong> ${formulario.mensaje}
-  `,
-  };
-
-  console.info("Enviando correo ...");
-  transporter.sendMail(mailOptions, (error, info) => {
+  console.info('Enviando correo ...')
+  transporter.sendMail(mailOptions, (error, info)=> {
     if (error) {
-      console.log(error);
+   console.log(error);
     } else {
-      console.log("Enviado! " + info.response);
+      console.log('Enviado! ' + info.response);
+
     }
   });
-};
+}
