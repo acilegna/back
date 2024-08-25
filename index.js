08c3676a5d75737e3c64199a0f9cf5287bea148f
acilegna <acilegna.airam88@gmail.com>
2024-08-24 23:14:17 -0600

    envio email funcional

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const configMensaje = require("./src/configMensaje");
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.post("/formulario", (req, res) => {
-  console.log(req.body)
-  configMensaje(req.body);
-  res.status(200).send();
-});
-
-app.get("/", (req, res) => {
-  res.send("pagina inicio desde indexxx");
-});
-
-app.listen(3000, () => {
-  console.log("Servidor corriendos ");
-});
-  
-
-
- 
-
-/* funciona bien !!
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
@@ -39,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(require('./routes/correoRoutes'));
 app.listen(3000, () => {
   console.log("Servidor corriendo");
-}); */
+}); 
 
  
 
